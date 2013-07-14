5c53c3e58604e1d7bab1701c1c3f8ac72ffc80cc
Silas Barta <sbarta@gmail.com>
2013-07-14 14:32:15 -0700

    testing for multiple valid values

--- PyvelyLetter/model.py
+++ PyvelyLetter/model.py
@@ -1,7 +1,27 @@
+from random import choice
+
+
 class Letter(object):
 
-    def __init__(self, text=""):
+    def __init__(self, text="", subs_dict=None):
         self.text = text
+        self.subs_dict = subs_dict
+        self.begin_delimiter = "{{"
+        self.end_delimiter = "}}"
+        #print self.begin_delimiter + "salutation" + self.end_delimiter
 
-    def apply_subs(self):
-        return self.text
+    def apply_subs(self, is_random=True):
+        output = self.text
+        if self.subs_dict:
+            for key, value in self.subs_dict.iteritems():
+                repl_from = self.begin_delimiter + str(key) + self.end_delimiter
+                if hasattr(value, '__iter__'):
+                    if is_random:
+                        repl_to = choice(value)
+                    else:
+                        repl_to = value[0]
+                else:
+                    repl_to = value
+                output = output.replace(repl_from, repl_to)
+                # print (self.begin_delimiter + str(key) + self.end_delimiter, str(value))
+        return output
--- test/test_subs.py
+++ test/test_subs.py
@@ -1,6 +1,9 @@
-import os
+from json import JSONDecoder
+from os.path import dirname, join
 import unittest
 from PyvelyLetter.model import Letter
+
+THIS_DIR = dirname(__file__)
 
 class TestSubs(unittest.TestCase):
 
@@ -11,9 +14,29 @@ class TestSubs(unittest.TestCase):
         super(TestSubs, self).tearDown()
 
     def test_const_letter(self):
-        with open(os.path.join(os.path.dirname(__file__), "const_letter.txt"), "r") as f:
+        with open(join(THIS_DIR, "const_letter.txt"), "r") as f:
             text = f.read().strip()
         ltr_obj = Letter(text)
         expected = "Hello World."
-        actual = ltr_obj.apply_subs()
+        actual = ltr_obj.apply_subs(is_random=False)
         self.assertEqual(expected, actual)
+
+    def test_one_choice_subs(self):
+        with open(join(THIS_DIR, "json1_letter.txt"), "r") as f:
+            text = f.read().strip()
+        with open(join(THIS_DIR, "input_dict.txt"), "r") as f:
+            subs_dict = JSONDecoder().decode(f.read())
+        expected = "Howdy,\nGood to see you."
+        ltr_obj = Letter(text, subs_dict)
+        actual = ltr_obj.apply_subs(is_random=False)
+        self.assertEqual(expected, actual)
+
+    def test_multi_choice(self):
+        with open(join(THIS_DIR, "json1_letter.txt"), "r") as f:
+            text = f.read().strip()
+        with open(join(THIS_DIR, "input_dict.txt"), "r") as f:
+            subs_dict = JSONDecoder().decode(f.read())
+        expecteds = ["{{salutation}},\nGood to see you.".replace("{{salutation}}", x) for x in ["Howdy", "Hi"]]
+        ltr_obj = Letter(text, subs_dict)
+        actual = ltr_obj.apply_subs()
+        self.assertTrue(actual in expecteds)
