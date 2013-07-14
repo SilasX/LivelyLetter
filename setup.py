c79681319f08a054baf38b7e6d9bbdd12654b417
Silas Barta <sbarta@gmail.com>
2013-07-14 15:42:25 -0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Module for generating numerous slightly-different versions of a given form letter',
+    'author': 'Silas Barta',
+    'url': 'None',
+    'download_url': 'None',
+    'author_email': 'sbarta@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['PyvelyLetter'],
+    'scripts': [],
+    'name': 'PyvelyLetter'
+}
+
+setup(**config)
