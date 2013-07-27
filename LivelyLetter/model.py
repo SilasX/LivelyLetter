67422a05bf3a3952ce47e39e6cff4d7ca3a0f34e
Silas Barta <sbarta@gmail.com>
2013-07-27 03:05:33 -0700

    minor tweak

--- LivelyLetter/model.py
+++ LivelyLetter/model.py
@@ -21,9 +21,9 @@ class Letter(object):
         self.obj_call_start = r'\[\[# '
         self.obj_call_end = r' #\]\]'
         self.obj_method_mark = r'#'
-        self.set_regexes()  # define all member vars that use the above four
-        self.partition_groups()
-        self.partition_object_calls()
+        self._set_regexes()  # define all member vars that use the above four
+        self._partition_groups()
+        self._partition_object_calls()
 
     def apply_subs(self, is_random=True, reset=True):
         if reset:
@@ -42,34 +42,6 @@ class Letter(object):
                 output = output.replace(repl_from, repl_to)
         self.working = output
         return output
-
-    def partition_groups(self):
-        """create the self.order_groups member variable to prepare for ordering theletter"""
-        group_pattern_cpd = re.compile(self.group_pattern_cpd_re)
-        self.order_groups = {}
-        # get patterns in the form [ (group name, text) ... ]
-        parsed_groups = re.findall(group_pattern_cpd, self.text)
-        for x in parsed_groups:
-            this_gp_pattern = self.group_name_start + x[0] + self.group_name_end + self.orderable_re
-            if this_gp_pattern in self.order_groups:
-                self.order_groups[this_gp_pattern].append(x[1])
-            else:
-                self.order_groups[this_gp_pattern] = [x[1]]
-
-    def partition_object_calls(self):
-        """create the self.object_calls member variable to prepare for inserting method invocations"""
-        object_pattern_cpd = re.compile(self.object_cpd_re)
-        # fill with key = pattern; value = {object: object name, method:method name}
-        self.object_calls = {}
-        # get patterns in the form [ (object name, method name) ... ]
-        parsed_objects = re.findall(object_pattern_cpd, self.text)
-        for x in parsed_objects:
-            this_obj_pattern = self.obj_call_start + x[0] + self.obj_method_mark + x[1] + self.obj_call_end
-            if this_obj_pattern not in self.object_calls:
-                self.object_calls[this_obj_pattern] = {
-                    'object': x[0],
-                    'method': x[1],
-                }
 
     def apply_ordering(self, reset=True):
         """return the result of applying a random ordering to the orderable parts """
@@ -109,10 +81,39 @@ class Letter(object):
         #print self.working
         return self.working
 
-    def set_regexes(self):
+    def _set_regexes(self):
         self.group_name_re = self.group_name_start + r'.+' + self.group_name_end #r'\[#.+#\]'  # surrounded by [# and #]
         self.orderable_re = self.group_pat_start + r'.+' + self.group_pat_end # r'\[\[.+\]\]'  # surrounded by [[ and ]]
         self.group_name_cpd_re = self.group_name_start + r'(?P<group_name>.+)' + self.group_name_end #r'\[#.+#\]'
         self.orderable_cpd_re = self.group_pat_start + r'(?P<group_text>.+)' + self.group_pat_end # r'\[\[.+\]\]'  # surrounded by [[ and ]]
         self.group_pattern_cpd_re = self.group_name_cpd_re + self.orderable_cpd_re
         self.object_cpd_re = self.obj_call_start + r'(?P<object_name>[^#]+)' + self.obj_method_mark + r'(?P<method_name>[^#]+)' + self.obj_call_end
+
+    def _partition_groups(self):
+        """create the self.order_groups member variable to prepare for ordering theletter"""
+        group_pattern_cpd = re.compile(self.group_pattern_cpd_re)
+        self.order_groups = {}
+        # get patterns in the form [ (group name, text) ... ]
+        parsed_groups = re.findall(group_pattern_cpd, self.text)
+        for x in parsed_groups:
+            this_gp_pattern = self.group_name_start + x[0] + self.group_name_end + self.orderable_re
+            if this_gp_pattern in self.order_groups:
+                self.order_groups[this_gp_pattern].append(x[1])
+            else:
+                self.order_groups[this_gp_pattern] = [x[1]]
+
+    def _partition_object_calls(self):
+        """create the self.object_calls member variable to prepare for inserting method invocations"""
+        object_pattern_cpd = re.compile(self.object_cpd_re)
+        # fill with key = pattern; value = {object: object name, method:method name}
+        self.object_calls = {}
+        # get patterns in the form [ (object name, method name) ... ]
+        parsed_objects = re.findall(object_pattern_cpd, self.text)
+        for x in parsed_objects:
+            this_obj_pattern = self.obj_call_start + x[0] + self.obj_method_mark + x[1] + self.obj_call_end
+            if this_obj_pattern not in self.object_calls:
+                self.object_calls[this_obj_pattern] = {
+                    'object': x[0],
+                    'method': x[1],
+                }
+
