ad1401d925e4d8e4441be4c2504d5e337347cc79
Silas Barta <sbarta@gmail.com>
2013-10-02 08:44:57 -0700

    full craigslist example

--- examples/craigslist_ad/maker.py
+++ examples/craigslist_ad/maker.py
@@ -5,14 +5,17 @@ from LivelyLetter import Letter
 
 
 class CLAd(object):
-    def random_string(self):
-        return sample(self.text.split())[0]
+    pass
+
+def random_string(s):
+    return sample(s.split(), 1)[0]
 
 ad_obj = CLAd()
 ad_obj.url = "http://example.com/craigslistadd_325"
 ad_obj.name = "Jake"
 ad_obj.text = """Get this 500 sq ft apartment for only $3000/month.  Only a 30 minute walk from public transit.  A steal for San Francisco!
 """
+ad_obj.blah = random_string(ad_obj.text)
 
 with open("templates/temp1.txt", "r") as f:
     letter_temp = f.read().strip()
@@ -20,9 +23,12 @@ with open("templates/temp1.txt", "r") as f:
 with open("jsons/ad1.json", "r") as f:
     subs_dict = JSONDecoder().decode(f.read())
 
-my_letter = Letter(letter_temp, subs_dict)
 clad_dict = {
     'ad': ad_obj,
 }
-my_letter.apply_all()
-print my_letter.apply_objects(clad_dict, reset=False)
+
+for x in range(3):
+    my_letter = Letter(letter_temp, subs_dict)
+    my_letter.apply_all()
+    print my_letter.apply_objects(clad_dict, reset=False)
+    print "######"
